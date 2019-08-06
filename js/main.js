28eb9179e23b4c9939a930d7beeb68268a44b61c
ferminrp <frodriguezpenelas@gmail.com>
2019-08-06 20:18:40 -0300

    no message

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 url = "https://api.sheety.co/8f53e7c5-555f-43ca-9e4b-cdaff3570ae0";
-
-
-
 $.getJSON(url, function(data) {
    var obj = data;
    $(obj).each(function(){
@@ -9,12 +6,13 @@ $.getJSON(url, function(data) {
      var link = this.url;
      var tagline = this.tagline;
      var image = this.image;
-
-     var html = '<div class="startup"><div class="startup-left"><img src="'+image+'" alt="'+tagline+'"></div><div class="startup-right"><h2>'+name+'</h2><p>'+tagline+'</p></div></div>'
-
+     var html = "<div onclick=\"openWebsite('"+link+"');\" class=\"startup\"><div class=\"startup-left\" style='background-image: url(\""+image+"\");'></div><div class=\"startup-right\"><h2>"+name+"</h2><p>"+tagline+"</p></div></div>"
+     /*var html = '<div class="startup"><div class="startup-left" style="background-image:url("'+image+'");"></div><div class="startup-right"><h2>'+name+'</h2><p>'+tagline+'</p></div></div>'*/
      $('.list').append(html);
-
-
   });
-
  });
+
+
+function openWebsite(a) {
+  window.open(a);
+};
